e8f3e8145cab4d134226b7913d8a18d4ec6fa710
Jerome Tang <jerometang@gmail.com>
2017-10-26 11:54:09 +0800

    add cloud config client

--- SpringCloudExample/eureka/service-example/src/main/java/com/jerometang/ServiceExampleApplication.java
+++ SpringCloudExample/eureka/service-example/src/main/java/com/jerometang/ServiceExampleApplication.java
@@ -3,6 +3,7 @@ package com.jerometang;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableConfigServer
 public class ServiceExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceExampleApplication.class, args);
